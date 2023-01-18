1d9acd91207e415b74e779c2b37439b5813a1506
carlopalazzi <57536121+carlopalazzi@users.noreply.github.com>
2023-01-18 09:59:00 +0000

    Update python_data_analysis.py

--- python_data_analysis.py
+++ python_data_analysis.py
@@ -2,12 +2,12 @@
 import pandas as pd
 # %%
 # import data
-df = pd.read_csv('Churn_Modelling.csv')
-df
+
+
 # %%
 # Count number of people from France
-french = df.loc[df['Geography'] == 'France']
-len(french)
+
+
 # %%
 # Any other questions?
 
